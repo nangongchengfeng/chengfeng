21421e83a8c5797b40814ae8a456aea3a88cd0a7
nangongchengfneg <1794748404@qq.com>
2019-08-09 17:26:34 +0800

    学生信息提交

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.user.mapper;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface UserMapper extends Mapper<UmsMember>{
+
+    List<UmsMember> selectAllUser();
+
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Autowired
+    UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Override
+    public List<UmsMember> getAllUser() {
+
+        List<UmsMember> umsMemberList = userMapper.selectAll();//userMapper.selectAllUser();
+
+        return umsMemberList;
+    }
+
+    @Override
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId) {
+
+        // 封装的参数对象
+        UmsMemberReceiveAddress umsMemberReceiveAddress = new UmsMemberReceiveAddress();
+        umsMemberReceiveAddress.setMemberId(memberId);
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.select(umsMemberReceiveAddress);
+
+
+//        Example example = new Example(UmsMemberReceiveAddress.class);
+//        example.createCriteria().andEqualTo("memberId",memberId);
+//        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
+
+        return umsMemberReceiveAddresses;
+    }
+}
